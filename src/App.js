d1c38f65ca653ec2aaa71a802153dcf8d3bbbaba
Konstantin Ivanov <konstiv23@gmail.com>
2020-04-29 21:13:35 +0300

    Added search input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,22 @@ function App() {
       <header>
         <h1>Where in the world?</h1>
       </header>
+      <main>
+        <div className="search-and-filter">
+          <div className="search">
+            <div className="search-icon-wrapper">
+              <i className="search-icon fas fa-search"></i>
+            </div>
+            <input type="text" placeholder="Search for a country..."/>
+          </div>
+          <select id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+      </main>
     </div>
   );
 }
