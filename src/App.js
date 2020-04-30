ebebd263f8677ea10b32f1829ae0712df68058c8
Konstantin Ivanov <konstiv23@gmail.com>
2020-04-30 13:37:15 +0300

    Moved search and filter in separate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CountryCard from "./CountryCard.js";
+import SearchAndFilter from "./SearchAndFilter.js";
 import "./App.css";
 
 function App() {
@@ -9,22 +10,7 @@ function App() {
         <h1>Where in the world?</h1>
       </header>
       <main>
-        <div className="search-and-filter">
-          <div className="search">
-            <div className="search-icon-wrapper">
-              <i className="search-icon fas fa-search"></i>
-            </div>
-            <input type="text" placeholder="Search for a country..."/>
-          </div>
-          <select className="world-region">
-            <option value="" disabled selected hidden>Filter by Region</option>
-            <option value="africa">Africa</option>
-            <option value="america">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-          </select>
-        </div>
+        <SearchAndFilter/>
         <div className="country-cards">
           <CountryCard/>
           <CountryCard/>
---
+++ src/SearchAndFilter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SearchAndFilter.css";
+
+function SearchAndFilter() {
+  return (
+    <div className="search-and-filter">
+      <div className="search">
+        <div className="search-icon-wrapper">
+          <i className="search-icon fas fa-search"></i>
+        </div>
+        <input type="text" placeholder="Search for a country..."/>
+      </div>
+      <select className="world-region">
+        <option value="" disabled selected hidden>Filter by Region</option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </div>
+  );
+}
+
+export default SearchAndFilter;
