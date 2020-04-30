311f4176c7a8c4c6b48e4e3099009102dfa59788
Konstantin Ivanov <konstiv23@gmail.com>
2020-04-30 18:23:54 +0300

    Updates

--- precache-manifest.996ceb74d37c9598303cb22d907a3ccb.js
+++ precache-manifest.996ceb74d37c9598303cb22d907a3ccb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14a21425fa089e5f723b2e14daa5928a",
+    "revision": "5346051137d509a61f5ffc0303436a49",
     "url": "/rest-countries/index.html"
   },
   {
-    "revision": "001c40cba1c1b60b0994",
+    "revision": "e501438e3073e42c3777",
     "url": "/rest-countries/static/css/main.58248139.chunk.css"
   },
   {
-    "revision": "f04e94bd4b4c3fc99a52",
-    "url": "/rest-countries/static/js/2.a31c6b93.chunk.js"
+    "revision": "75de193fd8268e12e870",
+    "url": "/rest-countries/static/js/2.463f23bc.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/rest-countries/static/js/2.a31c6b93.chunk.js.LICENSE.txt"
+    "url": "/rest-countries/static/js/2.463f23bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "001c40cba1c1b60b0994",
-    "url": "/rest-countries/static/js/main.49ae213c.chunk.js"
+    "revision": "e501438e3073e42c3777",
+    "url": "/rest-countries/static/js/main.d0fefc52.chunk.js"
   },
   {
     "revision": "d8b04d08b393a6bf339c",
