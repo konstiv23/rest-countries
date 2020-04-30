7c6128507af451afd799ee14e175ea05ef679e19
Konstantin Ivanov <konstiv23@gmail.com>
2020-04-30 14:44:58 +0300

    Added thousands comma separator to population

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -7,12 +7,16 @@ function CountryCard({flag, name, population, region, capital}) {
       <img src={flag} alt="flag"/>
       <div className="card-text">
         <h2>{name}</h2>
-        <div><strong>Population: </strong>{population}</div>
+        <div><strong>Population: </strong>{numberWithCommas(population)}</div>
         <div><strong>Region: </strong>{region}</div>
         <div><strong>Capital: </strong>{capital}</div>
       </div>
     </div>
   );
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default CountryCard;
