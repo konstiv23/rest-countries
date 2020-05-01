a86cb17fe27681e5d00a9ae5100cbf3bb18b5ea2
Konstantin Ivanov <konstiv23@gmail.com>
2020-05-01 17:39:08 +0300

    Changed h1's to links in cards

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import numberWithCommas from "./numberWithCommas.js";
 import "./CountryCard.css";
 
@@ -7,7 +8,7 @@ function CountryCard({flag, name, population, region, capital}) {
     <div className="CountryCard">
       <img src={flag} alt="flag"/>
       <div className="card-text">
-        <h2>{name}</h2>
+        <h2><Link to={"/rest-countries/details/" + encodeURI(name)}>{name}</Link></h2>
         <div><strong>Population: </strong>{numberWithCommas(population)}</div>
         <div><strong>Region: </strong>{region}</div>
         <div><strong>Capital: </strong>{capital}</div>
